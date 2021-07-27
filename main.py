37a3e79681a9789ac5e32c90b03b66862869be53
akshat71 <akshatnandan123@gmail.com>
2021-07-27 10:38:15 +0530

    Inversion Commit

---
+++ main.py
@@ -0,0 +1,42 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+a = input("ENTER Greetings: ")
+b = "HELLO"
+print(b)
+d = "WHATS YOUR NAME"
+print(d)
+c = input("MY NAME IS: ")
+e = "Hello" + c
+print(e)
+f = "WHICH GAME YOU PLAY: "
+print(f)
+g = input("I play: ")
+print(g)
+h = "Wow you play" + g
+print(h)
+j = input("WHATS YOUR FAV YTER? ")
+print(j)
+i = "WOW HE IS A GOOD YTER" + j
+print(i)
+k = "IT WAS NICE TALKING WITH YOU"
+print(k)
+l = "Bye!"
+print(l)
+m=input("WRITE BYE: ")
+print(m)
+n="Hope WILL MEET SOON!"
+print(n)
